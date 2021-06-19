94bcc8036a72897e8077aa4bc227527c68b0b711
Alex-Otero <alexanderotero2323@hotmail.com>
2021-06-18 19:12:16 -0500

    Cambios del 18/06/2021, resumen en el documento .txt

--- js/burger.js
+++ js/burger.js
@@ -5,23 +5,6 @@ if (iconoMenu){
 	iconoMenu.addEventListener('click', (e) => {
 		menu.classList.toggle('active_menu');
 		document.body.classList.toggle('opacity');
-
-		const rutaActual = e.target.getAttribute('src');
-
-		if(localStorage.getItem('dark-mode') === 'true'){
-			e.target.setAttribute('src', 'imágenes/open-menu2.png');
-		}
-		else {
-			e.target.setAttribute('src', 'imágenes/open-menu.png');
-		}
-
-		// if (rutaActual == 'imágenes/open-menu2.png'){
-		// 	e.target.setAttribute('src', 'imágenes/open-menu2.png');
-		// }
-
-		// else {
-		// 	e.target.setAttribute('src', 'imágenes/open-menu2.png');
-		// }
 	});
 
 
