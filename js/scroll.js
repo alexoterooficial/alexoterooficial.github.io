5781775b5aebc38a0339343917a0e13f46baf4e3
alexoterooficial <alexanderotero2323@hotmail.com>
2022-02-01 22:38:04 -0600

    Agregando portafolio

---
+++ js/scroll.js
@@ -0,0 +1,15 @@
+function inicio(){
+	document.getElementById('inicio').scrollIntoView();
+}
+
+function conocimientos(){
+	document.getElementById('conocimientos').scrollIntoView();
+}
+
+function proyectos(){
+	document.getElementById('proyectos').scrollIntoView();
+}
+
+function contacto(){
+	document.getElementById('contacto').scrollIntoView();
+}
