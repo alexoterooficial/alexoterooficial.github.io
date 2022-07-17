47664023c8910688d0b480c09fccca4fb528e142
alexoterooficial <alexanderotero2323@hotmail.com>
2022-07-17 13:26:52 -0500

    Changes

--- js/date.js
+++
@@ -1,5 +0,0 @@
-/*Esta página ha sido programada por Alex Otero. Todos los derechos son reservados*/
-
-document.write(new Date().getFullYear());
-
-/*Esta página ha sido programada por Alex Otero. Todos los derechos son reservados*/
--- js/main.js
+++ js/main.js
@@ -1,52 +1,17 @@
-const btnSwitch = document.querySelector('#switch');
-const btnSwitchNoMobile = document.querySelector('#switch-no_mobile');
+window.addEventListener("scroll", function(){var header = document.querySelector("header"); header.classList.toggle("sticky", window.scrollY > 0);})
 
-const menu_icon = document.getElementById('menu_icon');
-const menu = document.getElementById('menu');
-
-btnSwitch.addEventListener('click', () => {
-	document.body.classList.toggle('dark');
-	btnSwitch.classList.toggle('active');
-	btnSwitchNoMobile.classList.toggle('active');
-
-	if(document.body.classList.contains('dark')){
-		localStorage.setItem('dark-mode', 'true');
-	} else {
-		localStorage.setItem('dark-mode', 'false');
-	}
-});
-
-btnSwitchNoMobile.addEventListener('click', () => {
-	document.body.classList.toggle('dark');
-	btnSwitch.classList.toggle('active');
-	btnSwitchNoMobile.classList.toggle('active');
-
-	if(document.body.classList.contains('dark')){
-		localStorage.setItem('dark-mode', 'true');
-	} else {
-		localStorage.setItem('dark-mode', 'false');
-	}
-});
-
-
-if(localStorage.getItem('dark-mode') === 'true'){
-	document.body.classList.add('dark');
-	btnSwitch.classList.add('active');
-	btnSwitchNoMobile.classList.add('active');
-} else {
-	document.body.classList.remove('dark');
-	btnSwitch.classList.remove('active');
-	btnSwitchNoMobile.classList.remove('active');
+function inicio(){
+	document.getElementById('inicio').scrollIntoView();
 }
 
-function menu_f(){
-	menu.classList.toggle('active_menu');
-	menu_icon.classList.toggle('cross');
+function conocimientos(){
+	document.getElementById('conocimientos').scrollIntoView();
 }
 
-menu_icon.addEventListener("keyup", function(event) {
-	if (event.keyCode === 13) {
-		event.preventDefault();
-		menu_f();
-	}
-});
+function proyectos(){
+	document.getElementById('proyectos').scrollIntoView();
+}
+
+function contacto(){
+	document.getElementById('contacto').scrollIntoView();
+}
